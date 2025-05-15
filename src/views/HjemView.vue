70b320619211ae9502d26c4d917698fa6d547540
SofieCrille <149306559+SofieCrille@users.noreply.github.com>
2025-05-15 14:09:28 +0200

    .

--- src/views/HjemView.vue
+++ src/views/HjemView.vue
@@ -8,19 +8,11 @@
 
     <ion-content class="ion-padding">
       <!-- Make sure this is here! -->
-      <CardComponent
-        title="Velkommen"
-        subtitle="Start din dag"
-        content="Her kan du se dagens udfordringer, pointbalance, og andre vigtige oplysninger."
-        button-text="Se detaljer"
-        @button-click="onDetailsClick"
-      />
     </ion-content>
   </ion-page>
 </template>
 
 <script setup>
-import CardComponent from '@/components/CardComponent.vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
